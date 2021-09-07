23e43be7f8e5fbe6c99b1a65f4948f4e9785f59f
Rickyandikurniawan64 <39975146+rickyandi64@users.noreply.github.com>
2021-09-07 11:10:09 +0700

    Update tambahan Komentar
    Single Comment & Multiple Comment

--- Tugas_Keempat/src/tugas_4.kt
+++ Tugas_Keempat/src/tugas_4.kt
@@ -1,7 +1,15 @@
 fun main(args: Array<String>) {
-
+    /*Berikut ini data penyipanan Makanan Kesukaan
+    hasil Ouputnnya :
+    1. Ayam Geprek
+    2. Ayam Penyet
+    3. Ayam Bakar
+    4. Bakso
+    5. Martabak Telor
+    */
     val makanan_kesukaan = listOf("Ayam Geprek", "Ayam Penyet", "Ayam Bakar", "Bakso", "Martabak Telor")
     println("Berikut ini adalah Makanan kesukaan saya :")
+    // Berikut ini fungsi untuk menampilkan data yang ada di Val
     for ((index, makanan) in makanan_kesukaan.withIndex()) {
         println("${index + 1}. $makanan")
     }
